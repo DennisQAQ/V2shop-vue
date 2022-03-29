ee1b1a0edec7e90896e09acbe991e490b22f2c92
DennisQAQ <deardenniswang@gmail.com>
2022-03-29 22:34:19 +0800

    修改页面

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,8 +3,8 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">v2shop management</h2>
+          <p class="brand-info__intro">基于vue、element-ui构建开发，实现v2shop后台管理前端功能。</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
